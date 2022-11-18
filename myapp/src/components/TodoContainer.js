7a2836cb7c0ab4f61d77e7615dea4c383492fc48
misha <mikhail.dyoshin@gmail.com>
2022-11-18 15:49:39 +0300

    Updated handleChange method
    Now the method can apply changes that occure in the child TodoItem component to the completed field of todos state.

--- myapp/src/components/TodoContainer.js
+++ myapp/src/components/TodoContainer.js
@@ -25,9 +25,19 @@ class TodoContainer extends React.Component{
         ]
     };
 
-    // This method handles changes which occur in his child component
+    // This method handles changes which occur in his child component and aplly those changes to the state
     handleChange = (id) => {
-      console.log("clicked", id)
+      this.setState(prevState => ({
+        todos: prevState.todos.map(todo => {
+          if (todo.id === id) {
+            return {
+              ...todo,
+              completed: !todo.completed,
+            }
+          }
+          return todo
+        })
+      }))
     }
 
     // Showing all todos titles like a list on the page
@@ -35,7 +45,10 @@ class TodoContainer extends React.Component{
         return(
             <div>
                 <Header />
-                <TodosList todos={this.state.todos} handleChangeProps={this.handleChange}/>
+                <TodosList 
+                  todos={this.state.todos} 
+                  handleChangeProps={this.handleChange}
+                />
             </div>
         )
     }
