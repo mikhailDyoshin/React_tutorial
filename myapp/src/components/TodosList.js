fde9429ded7c58ac6af698df5441ea714aed307e
misha <mikhail.dyoshin@gmail.com>
2022-11-18 11:58:53 +0300

    Created TodoItem component and set a data flow between TodoItem and TodosList components

--- myapp/src/components/TodosList.js
+++ myapp/src/components/TodosList.js
@@ -1,10 +1,11 @@
 import React from "react";
+import TodoItem from "./TodoItem";
 
 class TodosList extends React.Component{
     render() {
         return (
             <ul>
-                {this.props.todos.map(todos => (<li>{todos.title}</li>))}
+                {this.props.todos.map(todo => (<TodoItem key={todo.id} todo={todo}/>))}
             </ul>
         )
     }
