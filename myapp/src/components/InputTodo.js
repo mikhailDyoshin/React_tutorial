8bd1305f92e5726a260c8b86f6a951f0ca6d9563
misha <mikhail.dyoshin@gmail.com>
2022-11-18 17:01:27 +0300

    Added an onChange method to the InputTodo component
    Now when one prints something to the form's input the onChange method logs hello word in the console.

--- myapp/src/components/InputTodo.js
+++ myapp/src/components/InputTodo.js
@@ -1,10 +1,27 @@
 import React from "react";
 
 class InputTodo extends React.Component {
+
+    // The state that InputTodo component holds
+    state = {
+        title: ""
+    }
+
+    // onChange method logs a "hello" word in the console when form's input changes
+    onChange = (e) => {
+        console.log("hello")
+    }
+
+
     render() {
         return (
             <form>
-                <input type="text" placeholder="Add Todo..."/>
+                <input 
+                    type="text" 
+                    placeholder="Add Todo..."
+                    value={this.state.title}
+                    onChange={this.onChange}
+                />
                 <button>Submit</button>
             </form>
         )
